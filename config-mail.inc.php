9e5fbc371a6de37ff1131cb05a3d4e0124dcdeaa
leabonaco <bonacorsilpro@gmail.com>
2025-11-14 11:27:59 +0100

    fichier de configuration de mail (version 1)

---
+++ config-mail.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$mail_host = 'smtp-etudaviz.alwaysdata.net';  
+$mail_port = 465;                    
+$mail_username = 'etudaviz@alwaysdata.net';
+$mail_password = 'DevWebetudaviz2526+';
+
+?>
