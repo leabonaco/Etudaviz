09f60ac5ae375b0114eb57de7d2bc9289a2ee2d5
leabonaco <bonacorsilpro@gmail.com>
2025-11-20 21:58:32 +0100

    Remove mail from repo and ignore it

--- config-mail.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mail_host = 'smtp-etudaviz.alwaysdata.net';  
-$mail_port = 465;                    
-$mail_username = 'etudaviz@alwaysdata.net';
-$mail_password = 'DevWebetudaviz2526+';
-
-?>
